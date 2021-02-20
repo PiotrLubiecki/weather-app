d97006e4de9d6043eea7e85b9cef8329c7191012
piotr13 <piotrek.lubiecki13@gmail.com>
2021-02-20 13:53:46 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-// import { weatherData } from "./weatherData"
+import { weatherData } from "./weatherData"
 
-// const citySearchButton = document.querySelector('#city-search-submit');
-//     citySearchButton.addEventListener('click', weatherData.displayWeather);
-// const searchInput = document.querySelector('#city-search')
-//     searchInput.addEventListener('keydown', function(event) {
-//         if(event.keyCode === 13){
-//             event.preventDefault();
-//             weatherData.displayWeather();
-//            }
-//     });
+const citySearchButton = document.querySelector('#city-search-submit');
+    citySearchButton.addEventListener('click', weatherData.displayWeather);
+const searchInput = document.querySelector('#city-search')
+    searchInput.addEventListener('keydown', function(event) {
+        if(event.keyCode === 13){
+            event.preventDefault();
+            weatherData.displayWeather();
+           }
+    });
 
-// const temperatureSlider = document.querySelector('.slider');
-//     temperatureSlider.addEventListener('click', () => {
-//         let currentTemperature = document.querySelector('#weather-temperature').textContent;
-//         const tempCheckbox = document.querySelector('#temp-checkbox');
-//         if (tempCheckbox.checked === false){
-//             currentTemperature = Math.round((currentTemperature - 32) / 9 * 5);
-//         } else {
-//             currentTemperature = Math.round(currentTemperature * 9 / 5 + 32);
-//         }
-//         document.querySelector('#weather-temperature').textContent = currentTemperature;
-//     });
+const temperatureSlider = document.querySelector('.slider');
+    temperatureSlider.addEventListener('click', () => {
+        let currentTemperature = document.querySelector('#weather-temperature').textContent;
+        const tempCheckbox = document.querySelector('#temp-checkbox');
+        if (tempCheckbox.checked === false){
+            currentTemperature = Math.round((currentTemperature - 32) / 9 * 5);
+        } else {
+            currentTemperature = Math.round(currentTemperature * 9 / 5 + 32);
+        }
+        document.querySelector('#weather-temperature').textContent = currentTemperature;
+    });
 
-// weatherData.displayWeather('Paris');
+weatherData.displayWeather('Paris');
--- src/weatherData.js
+++ src/weatherData.js
@@ -1,52 +1,59 @@
-// const weatherData = (() => {
-//     const weatherDataFetch = async function(city, desc, temp) {
-//         const currentCity = document.querySelector('#current-city');
-//         try {
-//             const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=4deeb2927ba731c378159029900bed51`, {mode: 'cors'})
-//             const cityData = await response.json();
-//             currentCity.textContent = city;
-//             let currentDescription = cityData.weather[0].main;
-//             desc.textContent = currentDescription;
-//             weatherImage(currentDescription);
-//             let currentTemperature = Math.round(cityData.main.temp) - 273;
-//             currentTemperature = tempDisplay(currentTemperature);
-//             temp.textContent = currentTemperature;
-//         } catch (error) {
-//             console.error(error);
-//             const alertDiv = document.querySelector('#alert-div');
-//             alertDiv.classList.add('alert-display');
-//             currentCity.textContent = '';
-//             temp.textContent = '';
-//             desc.textContent = '';
-//             setTimeout(() => {
-//                 alertDiv.classList.remove('alert-display');
-//             }, 4000);
-//         }
-//     }
+const weatherData = (() => {
+    const weatherDataFetch = async function(city, desc, temp) {
+        const currentCity = document.querySelector('#current-city');
+        const tempDiv = document.querySelector('#temperature');
+        const weatherDesc = document.querySelector('#weather-desc');
+        try {
+            const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=4deeb2927ba731c378159029900bed51`, {mode: 'cors'})
+            const cityData = await response.json();
+            if (currentCity.classList.contains('invisible')){
+                currentCity.classList.remove('invisible');
+                tempDiv.classList.remove('invisible');
+                weatherDesc.classList.remove('invisible');
+            }
+            currentCity.textContent = city;
+            let currentDescription = cityData.weather[0].main;
+            desc.textContent = currentDescription;
+            weatherImage(currentDescription);
+            let currentTemperature = Math.round(cityData.main.temp) - 273;
+            currentTemperature = tempDisplay(currentTemperature);
+            temp.textContent = currentTemperature;
+        } catch (error) {
+            console.error(error);
+            const alertDiv = document.querySelector('#alert-div');
+            alertDiv.classList.add('alert-display');
+            currentCity.classList.add('invisible');
+            tempDiv.classList.add('invisible');
+            weatherDesc.classList.add('invisible');
+            setTimeout(() => {
+                alertDiv.classList.remove('alert-display');
+            }, 4000);
+        }
+    }
     
-//     const displayWeather = function(city = '') {
-//         let selectedCity = document.querySelector('#city-search').value || city;
-//         const weatherDesc = document.querySelector('#weather-desc');
-//         const weatherTemp = document.querySelector('#weather-temperature');
-//         weatherDataFetch(selectedCity, weatherDesc, weatherTemp);
-//         document.querySelector('#city-search').value = '';
-//     }
+    const displayWeather = function(city = '') {
+        let selectedCity = document.querySelector('#city-search').value || city;
+        const weatherDesc = document.querySelector('#weather-desc');
+        const weatherTemp = document.querySelector('#weather-temperature');
+        weatherDataFetch(selectedCity, weatherDesc, weatherTemp);
+        document.querySelector('#city-search').value = '';
+    }
     
-//     const weatherImage = function(desc){
-//         const bckg = document.querySelector('html');
-//         bckg.style.backgroundImage = `Images/${desc}.jpg`;
-//     }
+    const weatherImage = function(desc){
+        const bckg = document.querySelector('html');
+        bckg.style.backgroundImage = `Images/${desc}.jpg`;
+    }
     
-//     const tempDisplay = function(temp){
-//         const tempCheckbox = document.querySelector('#temp-checkbox');
-//         if (tempCheckbox.checked === false){
-//             temp = Math.round(temp * 9 / 5 + 32);
-//         }
-//         return temp
-//     }
-//     return {
-//         displayWeather, 
-//     }
-// })()
+    const tempDisplay = function(temp){
+        const tempCheckbox = document.querySelector('#temp-checkbox');
+        if (tempCheckbox.checked === false){
+            temp = Math.round(temp * 9 / 5 + 32);
+        }
+        return temp
+    }
+    return {
+        displayWeather, 
+    }
+})()
 
-// export { weatherData }
+export { weatherData }
