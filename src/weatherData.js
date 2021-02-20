6be251ffc3eaa4b0f4ec13a8d7ae04a22f56a7e7
piotr13 <piotrek.lubiecki13@gmail.com>
2021-02-20 12:59:56 +0100

    initial commit

---
+++ src/weatherData.js
@@ -0,0 +1,52 @@
+// const weatherData = (() => {
+//     const weatherDataFetch = async function(city, desc, temp) {
+//         const currentCity = document.querySelector('#current-city');
+//         try {
+//             const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=4deeb2927ba731c378159029900bed51`, {mode: 'cors'})
+//             const cityData = await response.json();
+//             currentCity.textContent = city;
+//             let currentDescription = cityData.weather[0].main;
+//             desc.textContent = currentDescription;
+//             weatherImage(currentDescription);
+//             let currentTemperature = Math.round(cityData.main.temp) - 273;
+//             currentTemperature = tempDisplay(currentTemperature);
+//             temp.textContent = currentTemperature;
+//         } catch (error) {
+//             console.error(error);
+//             const alertDiv = document.querySelector('#alert-div');
+//             alertDiv.classList.add('alert-display');
+//             currentCity.textContent = '';
+//             temp.textContent = '';
+//             desc.textContent = '';
+//             setTimeout(() => {
+//                 alertDiv.classList.remove('alert-display');
+//             }, 4000);
+//         }
+//     }
+    
+//     const displayWeather = function(city = '') {
+//         let selectedCity = document.querySelector('#city-search').value || city;
+//         const weatherDesc = document.querySelector('#weather-desc');
+//         const weatherTemp = document.querySelector('#weather-temperature');
+//         weatherDataFetch(selectedCity, weatherDesc, weatherTemp);
+//         document.querySelector('#city-search').value = '';
+//     }
+    
+//     const weatherImage = function(desc){
+//         const bckg = document.querySelector('html');
+//         bckg.style.backgroundImage = `Images/${desc}.jpg`;
+//     }
+    
+//     const tempDisplay = function(temp){
+//         const tempCheckbox = document.querySelector('#temp-checkbox');
+//         if (tempCheckbox.checked === false){
+//             temp = Math.round(temp * 9 / 5 + 32);
+//         }
+//         return temp
+//     }
+//     return {
+//         displayWeather, 
+//     }
+// })()
+
+// export { weatherData }
