1d6002e9996653aab6b539862ce1633ae36853f7
piotr13 <piotrek.lubiecki13@gmail.com>
2021-02-20 13:00:41 +0100

    Initial dist subtree commit

---
+++ dist/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("// import { weatherData } from \"./weatherData\"\n\n// const citySearchButton = document.querySelector('#city-search-submit');\n//     citySearchButton.addEventListener('click', weatherData.displayWeather);\n// const searchInput = document.querySelector('#city-search')\n//     searchInput.addEventListener('keydown', function(event) {\n//         if(event.keyCode === 13){\n//             event.preventDefault();\n//             weatherData.displayWeather();\n//            }\n//     });\n\n// const temperatureSlider = document.querySelector('.slider');\n//     temperatureSlider.addEventListener('click', () => {\n//         let currentTemperature = document.querySelector('#weather-temperature').textContent;\n//         const tempCheckbox = document.querySelector('#temp-checkbox');\n//         if (tempCheckbox.checked === false){\n//             currentTemperature = Math.round((currentTemperature - 32) / 9 * 5);\n//         } else {\n//             currentTemperature = Math.round(currentTemperature * 9 / 5 + 32);\n//         }\n//         document.querySelector('#weather-temperature').textContent = currentTemperature;\n//     });\n\n// weatherData.displayWeather('Paris');\n\n//# sourceURL=webpack://weather-app/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
